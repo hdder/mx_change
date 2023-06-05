6362257b6b254576a10f22196e11c11950181385
dadahan <243128957@qq.com>
2023-06-05 18:13:46 +0800

    1.1可用版本 0.调试模式 1.注册 2.加全部课 3.加（正常的）全部课 4.加单个课（根据序号）5。删除账号 6.老师表

--- changeClassTool.py
+++ changeClassTool.py
@@ -3,6 +3,9 @@ import json
 import config
 import time
 from datetime import datetime, timedelta
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import Font
 def debugPrint(text):
     config.myPrint(text)
 
@@ -10,20 +13,28 @@ def decodeClassCode(text):
     return
 
 
-#入茶
+#获取token，并获取最新的课程列表
 def login():
+    url = "https://boke.mxnet.top/api/user/login"
+
     user = config.ADMIN_USERNAME
     password = config.ADMIN_PASSWORD
     token = config.ADMIN_TMP_TOKEN
-    url = config.MX_LOGIN_API
-    payloadToken = 'type=login&user={}&password={}&ltype=admin&token={}'.format(user, password, token)
+
+    payload = json.dumps({
+        "user": user,
+        "password": password,
+        "admin": True,
+        "token": token
+    })
+
     headers = {
-        'authority': 'zhibojian.mxnet.top',
+        'authority': 'boke.mxnet.top',
         'accept': '*/*',
         'accept-language': 'zh-CN,zh;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded',
-        'origin': 'https://zhibojian.mxnet.top',
-        'referer': 'https://zhibojian.mxnet.top/admin/pages/login/login',
+        'content-type': 'application/json',
+        'origin': 'https://boke.mxnet.top',
+        'referer': 'https://boke.mxnet.top/admin/',
         'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"macOS"',
@@ -33,86 +44,113 @@ def login():
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payloadToken)
+    response = requests.post(url, headers=headers, data=payload, proxies=None)
+    data = response.json()
 
-    # 将响应解析为Python对象
-    data = json.loads(response.text)
-
-    # 使用键"token"获取令牌
-    token = data["Info"]["token"]
-    config.ADMIN_TOKEN = token
-    debugPrint(token)
-    if token == "":
-        print("获取token失败")
-        return
-    else:
+    if data["code"] == 0:
+        config.ADMIN_TOKEN = data["token"]
         print("获取token成功")
+    else:
+        print("获取token失败")
 
+# 获取课程列表
+def get_lession_list():
+    print("最新课表获取中")
+    url = "https://boke.mxnet.top/api/room/select"
+    pages = 8  # 设置获取的总页数
 
-    # 请求课表
-    payloadClass = 'type=Get_RoomList&pagesnum=-1&search=undefined&token={}'.format(token)
+    config.MX_ALL_CLASS_LIST = []  # 重置 MX_ALL_CLASS_LIST 列表
+
+    for page in range(1, pages + 1):
+        payload = json.dumps({
+            "pages": page,
+            "token": config.ADMIN_TOKEN
+        })
+
+        headers = {
+            'authority': 'boke.mxnet.top',
+            'accept': '*/*',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'content-type': 'application/json',
+            'origin': 'https://boke.mxnet.top',
+            'referer': 'https://boke.mxnet.top/admin/',
+            'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+        }
+
+        response = requests.post(url, headers=headers, data=payload)
+        data = response.json().get('list', [])
+        for item in data:
+            if item['msgtime'] == 0:
+                msgtime = datetime.now()  # 设置默认时间为当前时间或其他默认值
+            else:
+                msgtime = datetime.fromtimestamp(item['msgtime'])
+            item['msgtime'] = msgtime.strftime('%Y-%m-%d %H:%M:%S')
+            config.MX_ALL_CLASS_LIST.append({
+                'ID': item['ID'],
+                'title': item['title'],
+                'msgtime': item['msgtime']
+            })
+
+    two_days_ago = datetime.now() - timedelta(days=2)
+
+    config.MX_RIGHT_CLASS_LIST = [
+        item for item in config.MX_ALL_CLASS_LIST
+        if datetime.strptime(item['msgtime'], '%Y-%m-%d %H:%M:%S') <= two_days_ago
+    ]
+    print("最新课表获取成功")
+    for index, item in enumerate(config.MX_ALL_CLASS_LIST):
+        item['index'] = index + 1
+    for index, item in enumerate(config.MX_RIGHT_CLASS_LIST):
+        item['index'] = index + 1
+
+#校验账号是否可用
+def check_user_available(user):
+    url = "https://boke.mxnet.top/api/user/v_user"
+
+    payload = json.dumps({
+        "user": user,
+        "token": config.ADMIN_TOKEN
+    })
     headers = {
-        'authority': 'zhibojian.mxnet.top',
+        'authority': 'boke.mxnet.top',
         'accept': '*/*',
         'accept-language': 'zh-CN,zh;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded',
-        'cookie': 'X_CACHE_KEY=b81354f65c9b0556b40356ce645ee000',
-        'origin': 'https://zhibojian.mxnet.top',
-        'referer': 'https://zhibojian.mxnet.top/admin/pages/user/list',
+        'content-type': 'application/json',
+        'origin': 'https://boke.mxnet.top',
+        'referer': 'https://boke.mxnet.top/admin/',
         'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payloadClass)
-
-    # 将响应解析为Python对象
-    debugPrint(response.text)
-    data = json.loads(response.text)
-    # 输出响应结果
-    # {"code":404,"message":"账号已存在"}
-    if data['code'] == 0:
-        print("获取课程成功!")
-        # 获取当前时间
-        current_time = datetime.now()
-
-        # 存储筛选后的数据
-        filtered_data = []
-
-        # 遍历列表中的字典元素
-        for item in data["list"]:
-            # 将 msgtime 转换为 datetime 对象
-            msgtime = datetime.fromtimestamp(item["msgtime"])
-
-            # 计算时间差
-            time_diff = current_time - msgtime
-
-            # 判断时间差是否小于 2 天
-            if time_diff.days < 2:
-                # 构造新的字典元素
-                filtered_item = {
-                    "ID": item["ID"],
-                    "title": item["title"],
-                    "msgtime": msgtime.strftime("%Y-%m-%d %H:%M:%S")
-                }
-
-                # 将筛选后的数据添加到列表中
-                filtered_data.append(filtered_item)
-                config.MX_RIGHT_CLASS_list = filtered_data
-    else:
-        print(f"获取课程失败，错误信息：{data['message']}")
-
-    return
-
-def regist():
+    response = requests.request("POST", url, headers=headers, data=payload)
+    result = response.json()
+    return result
+#注册账号
+def register_user():
     name = "name"
     user = input("请输入用户名：")
     password = input("请输入密码：")
-    token = config.ADMIN_TOKEN
+
+    while True:
+        user_check_result = check_user_available(user)
+
+        if user_check_result['code'] != 0:
+            error_message = user_check_result.get('message', '未知错误')
+            print("用户名不可用，错误信息：", error_message)
+            user = input("请重新输入用户名：")
+        else:
+            break
 
     url = "https://boke.mxnet.top/api/user/add"
 
@@ -122,7 +160,7 @@ def regist():
         "password": password,
         "role": "user",
         "note": "",
-        "token": token
+        "token": config.ADMIN_TOKEN
     })
 
     headers = {
@@ -152,78 +190,14 @@ def regist():
     else:
         error_message = result.get('message', '未知错误')
         print("请求失败，错误信息：", error_message)
-
-
-
-def getUserId():
-    token = config.ADMIN_TOKEN
-    url = config.MX_LOGIN_API
-    payload = 'type=Get_UserList&pagesnum={}&search=&token={}'.format("{}", token)
-    headers = {
-        'authority': 'zhibojian.mxnet.top',
-        'accept': '*/*',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded',
-        'cookie': 'X_CACHE_KEY=b81354f65c9b0556b40356ce645ee000',
-        'origin': 'https://zhibojian.mxnet.top',
-        'referer': 'https://zhibojian.mxnet.top/admin/pages/user/list',
-        'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36'
-    }
-
-    page_num = 1
-    data_list = []
-
-    index = 0
-    while True:
-        # 最多index次,防止ip被封
-        if index == 5:
-            print("达到请求保护次数{}，退出".format(index))
-            break
-        tmpPayload = payload.format(page_num)
-        debugPrint(tmpPayload)
-        response = requests.post(url, headers=headers, data=tmpPayload).text
-        response_json = json.loads(response)
-        debugPrint(response)
-        time.sleep(0.5)
-        if response_json['code'] != 0:
-            print(f"请求第{page_num}页失败")
-            break
-        data = response_json['list']
-        if not data:
-            print(f"已请求所有数据，共{len(data_list)}条，{page_num}页")
-            break
-        data1 = response_json['count']
-        if data1 == 0:
-            print(f"已请求所有数据，共{len(data_list)}条，{page_num}页")
-            break
-        data_list.extend(data)
-        page_num += 1
-        index += 1
-    # debugPrint(data_list)
-    config.MX_USER_DICT = data_list
-
-def add():
-    getUserId()
-    userId = input("账号：")
-    for data in config.MX_USER_DICT:
-        if data['user'] == userId:
-            debugPrint(f"目标用户 {userId} 的ID为 {data['ID']}")
-            break
-    else:
-        debugPrint(f"未找到用户 {userId}")
-
-#增加全部老师，试用
-def add_all_test():
+#增加全部老师，试用7天
+def add_right_lession():
     url = "https://boke.mxnet.top/api/subscribe/batchadd"
-
-    id_list = [data['ID'] for data in config.MX_RIGHT_CLASS_list]
-    extime = int((datetime.now() + timedelta(days=7)).timestamp()) * 1000
+    print("正常老师数")
+    print(len(config.MX_RIGHT_CLASS_LIST))
+    id_list = [data['ID'] for data in config.MX_RIGHT_CLASS_LIST]
+    days = int(input("请输入订阅的有效天数："))
+    extime = int((datetime.now() + timedelta(days=days)).timestamp()) * 1000
 
     payload = json.dumps({
         "ulist": [
@@ -257,3 +231,235 @@ def add_all_test():
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
+
+#增加全部老师
+def add_all_lession():
+    url = "https://boke.mxnet.top/api/subscribe/batchadd"
+    print("全部老师数")
+    print(len(config.MX_ALL_CLASS_LIST))
+    id_list = [data['ID'] for data in config.MX_ALL_CLASS_LIST]
+    days = int(input("请输入订阅的有效天数："))
+    extime = int((datetime.now() + timedelta(days=days)).timestamp()) * 1000
+
+    payload = json.dumps({
+        "ulist": [
+            config.MX_USERID
+        ],
+        "rlist": [
+            {
+                "ID": id,
+                "extime": extime
+            } for id in id_list
+        ],
+        "token":config.ADMIN_TOKEN
+    })
+
+    headers = {
+        'authority': 'boke.mxnet.top',
+        'accept': '*/*',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'content-type': 'application/json',
+        'origin': 'https://boke.mxnet.top',
+        'referer': 'https://boke.mxnet.top/admin/',
+        'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+
+#获取账号和uid对应对的字典
+def look_uid():
+    print("用户id表获取中")
+    url = "https://boke.mxnet.top/api/user/select"
+
+    token = config.ADMIN_TOKEN
+    total_pages = 10
+
+    headers = {
+        'authority': 'boke.mxnet.top',
+        'accept': '*/*',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'content-type': 'application/json',
+        'origin': 'https://boke.mxnet.top',
+        'referer': 'https://boke.mxnet.top/admin/',
+        'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+    }
+
+    result = {}  # 存储结果的字典
+
+    for page in range(1, total_pages + 1):
+        payload = json.dumps({
+            "query": "",
+            "pages": page,
+            "token": token
+        })
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        data = json.loads(response.text)
+        users = data["list"]  # 获取当前页的用户列表
+
+        for user in users:
+            user_id = user["ID"]
+            username = user["user"]
+            config.MX_USER_DICT[user_id] = username
+    print("用户id表获取成功")
+
+#根据用户名删除账号
+def delete_user():
+    # 用户输入账号
+    account = input("请输入要删除的账号：")
+    config.MX_USERID = ""
+    # 根据账号查找对应的 ID
+    user_id = None
+    for id, name in config.MX_USER_DICT.items():
+        if name == account:
+            user_id = id
+            break
+    # 如果找到了对应的 ID
+    if user_id:
+        print("用户id是")
+        print(user_id)
+        # 构建请求
+        url = "https://boke.mxnet.top/api/user/del"
+        payload = json.dumps({
+            "id": user_id,
+            "token": config.ADMIN_TOKEN
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Referer': 'https://boke.mxnet.top/admin/',
+            # 其他请求头字段...
+        }
+
+        # 发送请求
+        response = requests.post(url, headers=headers, data=payload)
+
+        # 处理响应
+        if response.status_code == 200:
+            # 成功删除用户
+            print("用户删除成功！")
+        else:
+            # 删除用户失败
+            print("用户删除失败！")
+    else:
+        # 未找到对应的账号
+        print("未找到要删除的账号！")
+
+#删单课
+def delete_lession():
+    # 用户输入账号
+    account = input("请输入要删除的账号：")
+    config.MX_USERID = ""
+    # 根据账号查找对应的 ID
+    user_id = None
+    for id, name in config.MX_USER_DICT.items():
+        if name == account:
+            config.MX_USERID = id
+            break
+    # 如果找到了对应的 ID
+    if user_id:
+        print("用户id是")
+        print(config.MX_USERID)
+
+#增加单课
+def add_lession():
+    # 用户输入账号
+    account = input("请输入要增加的账号：")
+    config.MX_USERID=""
+    # 根据账号查找对应的 ID
+    user_id = None
+    for id, name in config.MX_USER_DICT.items():
+        if name == account:
+            config.MX_USERID = id
+            break
+    # 如果找到了对应的 ID
+    if user_id:
+        print("用户id是")
+        print(config.MX_USERID)
+    url = 'https://boke.mxnet.top/api/subscribe/batchadd'
+    headers = {
+        'authority': 'boke.mxnet.top',
+        'accept': '*/*',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'content-type': 'application/json',
+        'origin': 'https://boke.mxnet.top',
+        'referer': 'https://boke.mxnet.top/admin/',
+        'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+    }
+
+    # 获取用户输入的老师序号
+    user_input = input("请选择你要的老师序号（以空格分隔）: ")
+    teacher_indexes = user_input.split()
+
+    # 获取用户输入的extime
+    extime_input = input("请选择你要的时间（1代表1年，2代表10年）: ")
+    extime_dict = {
+        '1': 365,  # 1年的天数
+        '2': 3650  # 10年的天数
+    }
+
+    current_time = datetime.now()
+    rlist = [{'id': config.MX_ALL_CLASS_LIST[int(index)]['ID'],
+              'extime': int((current_time + timedelta(days=extime_dict[extime_input])).timestamp() * 1000)} for index in
+             teacher_indexes]
+
+    data = {
+        'ulist': [config.MX_USERID],
+        'rlist': rlist,
+        'token': config.ADMIN_TOKEN
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    print(response.status_code)
+    print(response.json())
+
+#生成一个excel，展示现在所有的课程
+def get_excel_lession():
+    data = config.MX_ALL_CLASS_LIST
+    # 创建一个新的工作簿
+    wb = Workbook()
+    ws = wb.active
+
+    # 设置标题行
+    ws.append(['index', 'ID', 'title', 'msgtime'])
+
+    # 设置标题行的字体样式
+    font = Font(name='Arial', size=12, bold=True, color='FF0000')  # 设置字体为Arial，大小为12，粗体，红色
+    for cell in ws[1]:
+        cell.font = font
+
+    # 添加数据行
+    for row in data:
+        ws.append([row['index'], row['ID'], row['title'], row['msgtime']])
+
+    # 保存工作簿
+    wb.save('fancy_excel.xlsx')
+    # df = pd.DataFrame(data)  # 将字典数据转换为DataFrame
+    #
+    # # 将索引列放到第一列
+    # df.set_index('index', inplace=True)  # 使用 'index' 列作为索引
+    # df.reset_index(inplace=True)  # 重置索引，并将原索引列还原为普通列
+    #
+    # # 将DataFrame导出为Excel文件
+    # df.to_excel('output.xlsx', index=False)  # 文件名为output.xlsx，不包含索引列
--- main.py
+++ main.py
@@ -7,22 +7,35 @@ def main():
 
     # 登陆获取token
     changeClassTool.login()
+    changeClassTool.get_lession_list()
+    changeClassTool.look_uid()
     while True:
-        choice = input("请选择[1-3]: 0.调试模式 1.新用户试用 2.试用转套餐 3.老用户加课 4.删除客户 5.老客换课 6.帝王客户更新全部课程\n")
+        choice = input("请选择[1-6]:1.注册 2.加全部课 3.加（正常的）全部课 4.加单个课（根据序号）5。删除账号 6.老师表\n")
         "1。新客户试用：获取后台token；注册账号，取课程列表（取正常更新的老师），取系统时间-+7day，订阅所有课程，"
         "2。试用转套餐：输入账号；取消全部订阅，输入需要订阅的老师序号，取系统时间，设置到期时间"
         "3。老客户换课，输入账号，取"
         if choice == '0':
             config.DEBUG_MODE = 1
         elif choice == '1':
-            changeClassTool.regist()
-            # changeClassTool.add_all_test()
+            changeClassTool.register_user()
         elif choice == '2':
-            changeClassTool.add()
+            changeClassTool.look_uid()
+            changeClassTool.add_all_lession()
         elif choice == '3':
-            changeClassTool.login()
-            changeClassTool.get_roomlist()
-            print("你选择了功能3")
+            changeClassTool.look_uid()
+            changeClassTool.add_right_lession()
+        elif choice == '4':
+            changeClassTool.look_uid()
+            changeClassTool.add_lession()
+        elif choice == '5':
+            changeClassTool.look_uid()
+            changeClassTool.delete_user()
+        elif choice == '6':
+            changeClassTool.get_excel_lession()
+        elif choice == '7':
+            print(config.MX_ALL_CLASS_LIST)
+        elif choice == '8':
+            print(config.MX_RIGHT_CLASS_LIST)
         # ...
         else:
             print("选择无效，请重新选择")
