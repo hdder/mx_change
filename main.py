414c8a083e567863dedc17369b0f0fefcb17f2cf
dadahan <243128957@qq.com>
2023-06-05 18:13:46 +0800

    晚上修改好的，

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
 
     # 登陆获取token
     changeClassTool.login()
+    changeClassTool.get_lession_list()
+    changeClassTool.look_uid()
     while True:
         choice = input("请选择[1-3]: 0.调试模式 1.新用户试用 2.试用转套餐 3.老用户加课 4.删除客户 5.老客换课 6.帝王客户更新全部课程\n")
         "1。新客户试用：获取后台token；注册账号，取课程列表（取正常更新的老师），取系统时间-+7day，订阅所有课程，"
@@ -15,13 +17,15 @@ def main():
         if choice == '0':
             config.DEBUG_MODE = 1
         elif choice == '1':
-            changeClassTool.regist()
+            # changeClassTool.look_uid()
+            changeClassTool.register_user()
             # changeClassTool.add_all_test()
+            changeClassTool.look_uid()
+            changeClassTool.delete_user()
         elif choice == '2':
-            changeClassTool.add()
+            changeClassTool.delete_user()
         elif choice == '3':
             changeClassTool.login()
-            changeClassTool.get_roomlist()
             print("你选择了功能3")
         # ...
         else:
