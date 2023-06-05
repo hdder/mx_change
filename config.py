33f2bbe76a1716b79a5218d1bbbb5e12ba22652c
dadahan <243128957@qq.com>
2023-06-05 18:13:46 +0800

    1.0可用版本 0.调试模式 1.注册 2.加全部课 3.加（正常的）全部课 4.加单个课（根据序号）5。删除账号 6.老师表

--- config.py
+++ config.py
@@ -8,14 +8,15 @@ ADMIN_TOKEN = ""
 # 萌侠登陆接口
 MX_LOGIN_API = "https://zhibojian.mxnet.top/api"
 # 萌侠全部课程列表
-MX_ALL_CLASS_DICT = {}
+MX_ALL_CLASS_LIST = []
 # 萌侠正常更新的课程列表（最近两天有更新的）
 # {'ID': 851, 'title': '龙头部队尾pan', 'teaname': '', 'msgtime': '1970-01-01 08:00:00'}
-MX_RIGHT_CLASS_list =[]
+MX_RIGHT_CLASS_LIST =[]
 # 萌侠用户列表
-MX_USER_DICT = []
+MX_USER_DICT ={}
 
 MX_USERID= ""
+
 
 def myPrint(text):
     if DEBUG_MODE == 1:
